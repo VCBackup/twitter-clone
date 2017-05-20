e3fabe3042e77a9038217006da8dc2bfeef61fa6
VCBackup <dmoore1525@gmail.com>
2017-05-20 01:10:01 -0600

    Some changes,nothing notable, little progress.

--- js/animations.js
+++ js/animations.js
@@ -3,10 +3,20 @@ $(document).ready(function(){
         $('.tweet-compose').css('height', '5em');
         $('#tweet-controls').show();  
     })
+        var jsTweetCompose = document.getElementById('tweet-compose');
+        var jsCharCount = document.getElementById('char-count');
+        jsCharCount = 140;
     $('.tweet-compose').keydown(function(){
-        $('#char-count').val(10);
+        return jsCharCount.value--;        
     })
+    
+    $('.tweet').hover(function(){
+        $('.tweet-actions').fadeIn( 500 );
 
+    }, function(){
+        $('.tweet-actions').fadeOut( 100 );
+
+    })
 
 
 
